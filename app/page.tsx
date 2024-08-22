8111ea37a888a71db0be03d8e061eb55e89ad3c2
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2024-08-23 00:08:04 +0530

    feat(ui): shadcnui installation

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Tempo</h1>
       <p className="text-lg">Orchestrate Your Day</p>
+      <Button className="mt-4">Get Started</Button>
+
+      <ModeToggle />
     </div>
   );
 }
