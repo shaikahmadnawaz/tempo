61e977aef7d32060225cb0fc8ecbf31504f55910
Shaik Ahmad Nawaz <96189881+shaikahmadnawaz@users.noreply.github.com>
2024-09-10 01:18:20 +0000

    feat(auth): oauth sigin using auth.js and google

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { ModeToggle } from "@/components/mode-toggle";
+import SignIn from "@/components/sign-in";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
       <h1 className="text-4xl font-bold">Tempo</h1>
       <p className="text-lg">Orchestrate Your Day</p>
       <Button className="mt-4">Get Started</Button>
+      <SignIn />
 
       <ModeToggle />
     </div>
